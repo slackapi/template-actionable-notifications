d8b2accc32181a6609f529ec72904d03c906a349
Ankur Oberoi <aoberoi@gmail.com>
2017-09-19 09:50:43 -0700

    implement new tests for scenarios in shared channels

--- test/integration/in_shared_test.js
+++ test/integration/in_shared_test.js
@@ -16,10 +16,10 @@ const leveldown = require('leveldown');
 /*
  * Test suite
  */
-describe('app', function () {
+describe('app installed in a shared channel', function () {
   beforeEach(function (done) {
     process.env.SLACK_TOKEN = 'xoxp-FAKETOKEN';
-    process.env.SLACK_WEBHOOK = 'https://hooks.slack.com/services/T73MR4H8W/B73TY596K/FAKEWEBHOOK';
+    process.env.SLACK_WEBHOOK = 'https://hooks.slack.com/services/T73MR4H8W/B76D9498E/FAKEWEBHOOK';
     process.env.SLACK_VERIFICATION_TOKEN = 'FAKEVERIFICATIONTOKEN';
     level(testTicketStoreLocation, { valueEncoding: 'json' }, (error, db) => {
       if (error) return done(error);
@@ -28,48 +28,6 @@ describe('app', function () {
         .then(() => done())
         .catch(done)
     });
-  });
-
-  it('should post a Slack notification to the channel when a new incoming ticket is created', function () {
-    const newTicket = {
-      id: '1234',
-      link: 'https://somehelpdesk.localhost',
-      title: 'Test ticket',
-      description: 'Oh no! Something went wrong and now everything is on fire :fire:',
-      requester: 'Bob',
-      status: 'Open',
-      agent: 'Jane',
-      priority: 'High',
-    };
-    return startReplayForScenario('new_incoming_ticket')
-      .then(() => sendIncomingTicket(newTicket))
-      .then(waitOneSecond)
-      .then(() => new Promise((resolve, reject) => {
-        // Verify the ticket was stored
-        this.ticketStore.get(newTicket.id, (error) => {
-          if (error) { return reject(error); } resolve();
-        });
-      }))
-      .then(stopReplay)
-      .then((history) => {
-        // Assert on metadata of the interaction to ensure completion of the replay
-        assert.equal(history.meta.unmatchedCount.incoming, 0);
-        assert.equal(history.meta.unmatchedCount.outgoing, 0);
-
-        // Feel free to be as specific...
-        assert.equal(history.interactions.length, 1, 'number of interactions');
-        const requestBody = JSON.parse(history.interactions[0].request.body);
-        assert.equal(requestBody.attachments.length, 1, 'number of attachments in request body');
-        const attachment = requestBody.attachments[0];
-        assert.equal(attachment.title, newTicket.title, 'attachment title');
-        assert.equal(attachment.title_link, newTicket.link, 'attachment title link');
-        assert.equal(attachment.text, newTicket.description, 'attachment text');
-        assert.equal(attachment.callback_id, newTicket.id, 'attachment callback id');
-
-        // ... or generic as makes sense for your app.
-        assert.equal(attachment.fields.length, 4, 'attachment number of fields');
-        assert.equal(attachment.actions.length, 3, 'attachment number of actions');
-      });
   });
 
   describe('after a new incoming ticket is created', function () {
@@ -94,12 +52,12 @@ describe('app', function () {
     });
 
     it('should save the ticket, update the interactive message, DM the new assignee, and update the notification ' +
-       'channel when a user claims a ticket from an interactive message', function () {
-      const userWhoPressedClaimButton = 'ankur';
-      const messageUpdateUrl = '/actions/T73MR4H8W/242143869092/FAKEACTION';
+       'channel when an external user claims a ticket from an interactive message', function () {
+      const userWhoPressedClaimButton = 'aoberoi_sc_ankur_exte';
+      const messageUpdateUrl = '/actions/T73MR4H8W/243622838500/FAKEACTION';
       const dmUrl = '/api/chat.postMessage'
       const notificationsUrl = url.parse(process.env.SLACK_WEBHOOK).path;
-      return startReplayForScenario('user_claims_ticket')
+      return startReplayForScenario('user_claims_ticket_in_shared')
         .then(waitOneSecond)
         .then(() => new Promise((resolve, reject) => {
           // verify the ticket was saved
@@ -140,11 +98,11 @@ describe('app', function () {
 
     it('should save the ticket, update the interactive message, DM the new assignee, and update the notification '+
        'channel when a user assigns a ticket from an interactive message', function () {
-      const userWhoIsAssigned = 'slackbot';
-      const messageUpdateUrl = '/actions/T73MR4H8W/242760108100/FAKEACTION';
+      const userWhoIsAssigned = 'aoberoi_sc_bob_extern';
+      const messageUpdateUrl = '/actions/T73MR4H8W/243812058389/FAKEACTION';
       const dmUrl = '/api/chat.postMessage'
       const notificationsUrl = url.parse(process.env.SLACK_WEBHOOK).path;
-      return startReplayForScenario('user_assigns_ticket')
+      return startReplayForScenario('user_assigns_ticket_in_shared')
         .then(waitOneSecond)
         .then(() => new Promise((resolve, reject) => {
           // verify the ticket was saved
@@ -183,22 +141,22 @@ describe('app', function () {
         });
     });
 
-    it('should save the ticket, update the interactive message, and update the notification channel when a user ' +
-       'changes a ticket\'s priority from an interactive message', function () {
-      const newPriority = 'Low';
-      const messageUpdateUrl = '/actions/T73MR4H8W/242851266277/FAKEACTION';
+    it('should save the ticket, update the interactive message, DM the new assignee, and update the notification '+
+      'channel when an external user assigns a ticket to an external user from an interactive message', function () {
+      const userWhoIsAssigned = 'aoberoi_sc_bob_extern';
+      const messageUpdateUrl = '/actions/T73MR4H8W/243007753920/FAKEACTION';
       const dmUrl = '/api/chat.postMessage'
       const notificationsUrl = url.parse(process.env.SLACK_WEBHOOK).path;
-      return startReplayForScenario('user_changes_ticket_priority')
+      return startReplayForScenario('external_user_assigns_ticket_in_shared')
         .then(waitOneSecond)
         .then(() => new Promise((resolve, reject) => {
           // verify the ticket was saved
           this.ticketStore.get(this.ticket.id, (error, ticket) => {
             if (error) { return reject(error); }
-            if (ticket.fields.priority === newPriority) {
+            if (ticket.fields.agent === userWhoIsAssigned) {
               resolve();
             } else {
-              reject('The priority which was selected was not assigned');
+              reject('The user who was selected was not assigned');
             }
           });
         }))
@@ -211,13 +169,20 @@ describe('app', function () {
           const update = history.interactions.find(interaction => interaction.request.url === messageUpdateUrl);
           const updateContents = JSON.parse(update.request.body);
           const attachment = updateContents.attachments[0];
-          const field = attachment.fields.find(field => field.title === 'Priority');
-          assert.equal(field.value, newPriority, 'interactive message updated');
+          const field = attachment.fields.find(field => field.title === 'Agent');
+          assert.equal(field.value, userWhoIsAssigned, 'interactive message updated');
+
+          // verify the assignee was DMed
+          const dm = history.interactions.find(interaction => interaction.request.url === dmUrl);
+          const dmContents = qs.parse(dm.request.body);
+          assert.equal(dmContents.text, 'You\'ve been assigned the following ticket: ');
+          const attachments = JSON.parse(dmContents.attachments);
+          assert.equal(attachments.length, 1);
 
           // verify notification channel was updated
           const notification = history.interactions.find(interaction => interaction.request.url === notificationsUrl);
           const notificationContents = JSON.parse(notification.request.body);
-          assert.notEqual(notificationContents.text.indexOf(`Priority is now ${newPriority}`), -1);
+          assert.notEqual(notificationContents.text.indexOf(`Agent ${userWhoIsAssigned} is now assigned`), -1);
         });
     });
   });
